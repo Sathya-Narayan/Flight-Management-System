8b1202bdff5db658d8df7e36cff5c98d2d00dc5b
Sathya-Narayan <sathya.chester007@gmail.com>
2022-01-18 11:30:19 +0530

    delete

--- FlightMgmtSystemFRONTEND/src/app/Shared/Model/Flight.ts
+++
@@ -1,8 +0,0 @@
-export class Flight {
-  FlightId: number;
-  FlightName: string;
-  public constructor(FlightId, FlightName) {
-    this.FlightId = FlightId;
-    this.FlightName = FlightName;
-  }
-}
